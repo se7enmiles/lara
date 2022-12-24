8a3f67b9ef397ab1bf7bd3b8103e2ef1a897e829
se7enmiles <seven.miles.x@gmail.com>
2022-12-24 14:19:01 +0400

    add caching for single post cleanup routes file

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,12 @@ Route::get('/', function () {
 });
 
 Route::get('/posts/{post}', function ($slug) {
-    $path = __DIR__."/../resources/posts/{$slug}.html";
 
-    if (!file_exists($path)){
+    if (!file_exists($path = __DIR__."/../resources/posts/{$slug}.html")){
         return redirect('/');
     }
-    $post = file_get_contents($path);
+
+    $post = cache()->remember("posts.{$slug}", 5, fn()=>file_get_contents($path));
 
     return view('post', ['post'=>$post]);
 
